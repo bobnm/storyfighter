2c8fc1d1c5fb3969c5a8fde6246114880c526f11
Bob <bobnmfrog@gmail.com>
2014-10-11 14:51:07 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storyfighter::Application.config.secret_key_base = '04705ccb66f16c2022b931d78e0b30a768235f00d524b29d8b405d06cfe9210a63e906cbb6a9908052d3e108c5ea424caa1181850d6a0570d7c2414febc18b29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Storyfighter::Application.config.session_store :cookie_store, key: '_storyfighter_session'
